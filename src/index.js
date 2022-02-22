1898e527f931e1fa767b5f9c467a8c1bc3f4bea5
Disboard <20719424+Hoohyun@users.noreply.github.com>
2022-02-22 17:29:52 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 // Require the necessary discord.js classes
-
-const Discord = require('discord.js')
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] })
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // const config = require('./config.json');
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
-});
-
-// when bot is pinged reply pong
-client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
-    }
 });
 
 // Login to Discord with your client's token
