cd4a9095e81c58cc2dfef1294cb00f844e394c82
Eric <erichklee43@gmail.com>
2022-02-22 16:42:29 +1100

    Adding the rest of the files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+
+const Discord = requiere('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -18,4 +20,4 @@ client.on("message", msg => {
 });
 
 // Login to Discord with your client's token
-client.login(token);
+client.login(config.token);
