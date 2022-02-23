b21c93fa295f1f0e0bdaa691f6f1263ac1a41a32
Hayton L <46881618+Malxyzia@users.noreply.github.com>
2022-02-23 11:14:45 +1100

    Attempting chat command implementation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const channel = client.channels.cache.get('id');
-
-
+const prefix = "!";
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('PRTS is Online.');
@@ -22,6 +21,20 @@ client.on('interactionCreate', async interaction => {
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	}
+});
+
+client.on("message", async (message) => {
+
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	let command = message.content.split(" ")[0];
+	command = command.slice(0, 1);
+
+	if (command === "ping") {
+		message.channel.send("Pong!");
+	}
+
 });
 
 // Login to Discord with your client's token
